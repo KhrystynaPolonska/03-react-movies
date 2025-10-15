1f29c99c882b2ac37b380cff60b61d2bc24fd0ff
kristy <krisposol7@gmail.com>
2025-10-15 18:21:37 +0200

    Update MovieGrid.tsx

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -9,32 +9,27 @@ interface MovieGridProps {
 
 const MovieGrid = ({ movies, onSelect }: MovieGridProps) => {
   return (
-    <>
-      <ul className={css.grid}>
-        {movies.length > 0 &&
-          movies.map((movie) => {
-            const { id, poster_path, title } = movie;
-            return (
-              <li
-                onClick={() => {
-                  onSelect(movie);
-                }}
-                key={id}
-              >
-                <div className={css.card}>
-                  <img
-                    className={css.image}
-                    src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
-                    alt="movie title"
-                    loading="lazy"
-                  />
-                  <h2 className={css.title}>{title}</h2>
-                </div>
-              </li>
-            );
-          })}
-      </ul>
-    </>
+    <ul className={css.grid}>
+      {movies.length > 0 &&
+        movies.map((movie) => {
+          const { id, poster_path, title } = movie;
+          return (
+            <li
+              onClick={() => onSelect(movie)}
+              key={id}
+              className={css.card}
+            >
+              <img
+                className={css.image}
+                src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+                alt={title} 
+                loading="lazy"
+              />
+              <h2 className={css.title}>{title}</h2>
+            </li>
+          );
+        })}
+    </ul>
   );
 };
 
